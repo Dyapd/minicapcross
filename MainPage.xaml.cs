d7919a5a4c7f5065b891d76d47e563fbbf2f5db8
Allen Payad <allenpayad1@gmail.com>
2024-11-17 15:51:10 +0800

    a

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,20 +1,70 @@
-﻿namespace test
+﻿using Microsoft.Maui.Controls;
+using System.Data.SqlClient;
+
+namespace test
 {
     public partial class MainPage : ContentPage
     {
+        private const string AdminContactNumber = "011504";
+        private const string AdminPassword = "1234";
+        string connectionString = "Data Source=DESKTOP-TU0VOQH\SQLEXPRESS;Initial Catalog=Register;Integrated Security=True";
 
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private async void OnLoginClicked(object sender, EventArgs e)
+        private void OnLoginClicked(object sender, EventArgs e)
         {
-            //lagay dito ung login backend!
-            //placeholder if else lng to para mapagana ko ung navigational page remove if lalagay na
+            string enteredEmail = "Allen";
+            string enteredPassword = "Payad";
 
-            await Navigation.PushAsync(new StudentDashboard());
+
+            if (enteredEmail == AdminContactNumber && enteredPassword == AdminPassword)
+            {
+                //Insert Admin Page Method Here
+            }
+            else
+            {
+                if (CheckRegisteredAccount(enteredEmail, enteredPassword))
+                {
+                    //Insert Student Page Method Here
+                }
+                else
+                {
+                    // MessageBox.Show("Invalid credentials.");
+                }
+            }
+        }
+        private bool CheckRegisteredAccount(string enteredEmail, string enteredPassword)
+        {
+            string query = "SELECT 1 FROM StudentUsers WHERE Student_Email = @Email AND Student_Password = @Password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Email", enteredEmail);
+                        command.Parameters.AddWithValue("@Password", enteredPassword);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            return reader.HasRows;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    return false;
+                }
+            }
+        }
+        private void StudentDashboardLogin(string Student_Password, string Student_Email)
+        {
+
         }
     }
-
 }
