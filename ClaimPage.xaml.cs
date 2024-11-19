bdd9500a20b6cf814446b038487524ba987c947c
Allen Payad <allenpayad1@gmail.com>
2024-11-19 14:10:38 +0800

    -added claim page skeleton ui

--- ClaimPage.xaml.cs
+++ ClaimPage.xaml.cs
@@ -6,4 +6,18 @@ public partial class ClaimPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnClickedClaimBtn(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+        // Get the selected item from the Picker
+        var selectedOption = comboBox.SelectedItem as string;
+
+        // Update the label with the selected option
+        test.Text = $"You selected: {selectedOption}";
+    }
 }
