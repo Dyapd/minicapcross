203301159f4cb28f6555e6e4374041ca80c9da3e
Allen Payad <allenpayad1@gmail.com>
2024-11-19 15:49:02 +0800

    login and admin dashboard UI stuffs

--- AdminDashboard.xaml.cs
+++ AdminDashboard.xaml.cs
@@ -6,4 +6,35 @@ public partial class AdminDashboard : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	public async void OnClickedLogoutBtn(object sender, EventArgs e)
+	{
+		Application.Current.MainPage = new NavigationPage(new MainPage());
+        await Navigation.PopAsync();
+	}
+    public async void OnClickedReportsBtn(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new MainPage());
+        await Navigation.PopAsync();
+    }
+    public async void OnClickedItemsBtn(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new MainPage());
+        await Navigation.PopAsync();
+    }
+    public async void OnClickedSubmitFormBtn(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new MainPage());
+        await Navigation.PopAsync();
+    }
+    public async void OnClickedClaimsBtn(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new MainPage());
+        await Navigation.PopAsync();
+    }
+    public async void OnClickedLogsBtn(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new MainPage());
+        await Navigation.PopAsync();
+    }
 }
