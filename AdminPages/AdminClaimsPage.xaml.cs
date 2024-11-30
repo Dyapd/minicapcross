c85b4ec7651c1cc32f6a8bb91c0a5bcd82839477
PhantomFury24 <charlesedwin098@gmail.com>
2024-11-30 13:52:02 +0800

    admin claims page

--- AdminPages/AdminClaimsPage.xaml.cs
+++ AdminPages/AdminClaimsPage.xaml.cs
@@ -1,11 +1,37 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
-using Microsoft.Data.SqlClient;
-namespace test.Pages;
-
-public partial class AdminClaimsPage : ContentPage
+namespace test.Pages
 {
-	public AdminClaimsPage()
-	{
-		InitializeComponent();
-	}
+    public partial class AdminClaimsPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+        public ICommand DetailsCommand { get; }
+
+        public AdminClaimsPage()
+        {
+            InitializeComponent();
+
+            
+            Items = new ObservableCollection<string>
+            {
+                "Claim 1",
+                "Claim 2",
+                "Claim 3",
+                "Claim 4",
+                "Claim 5"
+            };
+
+            
+            DetailsCommand = new Command<string>(OnDetailsClicked);
+
+            BindingContext = this;
+        }
+
+        
+        private void OnDetailsClicked(string claim)
+        {
+            DisplayAlert("Details", $"Details for {claim}", "OK");
+        }
+    }
 }
